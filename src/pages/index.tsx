22828a3fef9d61ec391046b558dc9c3e6b3e82f2
Chiaki203 <42350913+Chiaki203@users.noreply.github.com>
2022-11-14 18:29:21 +0100

    Add todo list component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,27 +2,32 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import {signIn, signOut, useSession} from 'next-auth/react'
 
-const Home:NextPage = () => {
+import { TodoList } from '~/components/TodoList';
+
+const Index:NextPage = () => {
   const {data:session} = useSession()
   if (session) {
     return (
-      <>
-        signed in as {session.user?.email}<br/>
-        <button onClick={() => signOut()} >Sign out</button>
-      </>
+      <div className='container mx-auto p-4'>
+        Signed in as {session.user?.email} <br/>
+        <button className='p-3 bg-gray-300' onClick={() => signOut()}>
+          Sign out
+        </button>
+        <TodoList/>
+      </div>
     )
   }
   return (
-    <>
+    <div className='container mx-auto p-4'>
       Not signed in <br/>
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+      <button className='p-3 bg-gray-300' onClick={() => signIn()} >
+        Sign in
+      </button>
+    </div>
   )
 }
 
-export default Home
-
-
+export default Index
 
 
 
